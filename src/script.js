912c78b9a6e1c98b24718a9197512f434e5ab68f
TrishaGovind <trisha.govind@gmail.com>
2021-10-18 17:51:42 +0800

    Update script.js

--- src/script.js
+++ src/script.js
@@ -78,22 +78,3 @@ function formatDate(date) {
 
 let currentDay = new Date();
 document.querySelector("#date").innerHTML = formatDate(currentDay);
-
-function formatTime(timestamp) {
-  let time = new Date(timestamp);
-
-  let hours = time.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = time.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  return `${hours}:${minutes}`;
-}
-
-document.querySelector("#update-sunrise").innerHTML = formatTime(
-  response.data.sys.sunrise * 1000
-);
