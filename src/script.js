47e123921f39c981fe3cabe4fe9831aeb1287a59
TrishaGovind <trisha.govind@gmail.com>
2021-10-18 12:05:53 +0800

    First import

---
+++ src/script.js
@@ -0,0 +1,80 @@
+function cityUpdate(event) {
+  event.preventDefault();
+  let apiKey = "0dc40d3d7cda209ca40e77430c74cf57";
+  let city = document.querySelector("#search-city").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", cityUpdate);
+
+function showTemp(response) {
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#day-high").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#day-low").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let units = `metric`;
+  let apiKey = "0dc40d3d7cda209ca40e77430c74cf57";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showTemp);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let button = document.querySelector("#location-id");
+button.addEventListener("click", getCurrentPosition);
+
+function formatDate(date) {
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${date.getMinutes()}`;
+  }
+
+  return `${day}, ${hour}:${minutes}`;
+}
+
+let currentDay = new Date();
+document.querySelector("#date").innerHTML = formatDate(currentDay);
